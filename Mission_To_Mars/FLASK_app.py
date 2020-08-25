c517901e6d40c8f4d6a9fc65215337ef9dcccb90
Jay Sueno <jay.sueno@gmail.com>
2020-08-24 20:54:17 -0700

    finished all the code portion of the assignment

--- Mission_To_Mars/FLASK_app.py
+++ Mission_To_Mars/FLASK_app.py
@@ -17,18 +17,24 @@ app = Flask(__name__)
 # TEST Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
+# Create the index/home page route that will connect to the MongoDB with your scraped data and then define it for your html code
 @app.route("/")
 def index():
-
+    # connect no-sql database with PyMongo
     mars_data = mongo.db.mars_data.find_one()
+    # Use render_templates() to connect your database with your html file and render the desired data into my dashboard
     return render_template("index.html", mars=mars_data)
 
-
+# Create a route to your scrape function you created scrape_mars.py and upload the returned dictionary to the mongodb database
 @app.route("/scrape")
 def scraper():
+    # define the database collection and connect to mongo db
     mars_data = mongo.db.mars_data
+    # call the scrape() function you imported above 
     scraped_data = scrape_mars.scrape()
+    # load the scraped dictionary into mongodb using ".update()" 
     mars_data.update({}, scraped_data, upsert=True)
+    # afterwards we redirected it to our home/index dashboard to populate the HTML 
     return redirect("/", code=302)
 
 
